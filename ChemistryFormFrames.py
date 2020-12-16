0d4f7aeb66cd6cc85ad8fa9a7369c43ef00b33f8
Gordon Pettus <38599393+GLPUSNA67@users.noreply.github.com>
2020-12-15 23:07:49 -0800

    Update ChemistryFormFrames.py

--- ChemistryFormFrames.py
+++ ChemistryFormFrames.py
@@ -14,7 +14,7 @@ labelfont= ('Ariel', 12) #, 'bold')
 buttonfont= ('Ariel', 12) #, 'bold')
 entryfont= ('Ariel', 12) #, 'bold')
 
-# *** Start constants and variables
+# *** Start descriptions and list of constants and variables
 elements = "Ac Ag Al Am Ar As At Au B Ba Be Bi Bk Br C Ca Cd Ce Cf Cl Cm Co Cr Cs Cu Dy Er Es Eu" \
  "F Fe Fm Fr Ga Gd Ge H He Hf Hg Ho I In Ir K Kr La Li Lu Md Mn Mo N Na Nb Nd Ne Ni Np O Os " \
  "P Pa Pb Pd Pm Po Pr Pt Pu Ra Rb Re Rh Rn  Ru S Sb Sc Se Si Sm Sn Sr Ta Tb Tc Te Th Ti Tl Tm" \
