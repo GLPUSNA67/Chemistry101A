a51c57a8557ba0b9d4ed9c7668dc4e55a2f368eb
Gordon Pettus <38599393+GLPUSNA67@users.noreply.github.com>
2020-12-27 23:09:55 -0800

    Made a names dictionary for ions -- similar to those for elements and compounds -- and added it to main ChemistryFormFrames.py

---
+++ Make_Name_Ion_Dict.py
@@ -0,0 +1,33 @@
+'''sample list from elements
+elements_symbols_list = ['Ac', 'Ag', 'Al']
+'''
+ion_list = ['C2H3O2', 'ClO2', 'ClO3', 'ClO4', 'CN', 'CO32', 'CuS', 'FeCl2', 'FeCl3', 'H2PO4', 'HCO3',
+            'Hg2O', 'HgO', 'H3O', 'HPO42', 'HSO4', 'OH', 'NH4', 'NO3', 'NO2', 'MnO4', 'O22', 'SO42',
+            'SO32', 'PO43']
+
+ion_elements_list = ['Zn2', 'Cr3', 'K1', 'Mg2']
+
+'''
+compounds_names_dict = {'aluminum_carbide': 'Al4C3'}
+{'C2H3O2 = dict(formula= 'C2H3O2-', elements= 'CHO', name= 'acetate', charge= '-1'}
+'''
+
+ion__names_dict = {'acetate': 'C2H3O2', 'chlorite': 'ClO2', 'chlorate': 'ClO3', 'perchlorate': 'ClO4',
+                   'cyanide': 'CN', 'carbonate': 'CO32', 'copper_(II)_sulfide': 'CuS', 'iron_(II)_chloride': 'FeCl2',
+                   'iron_(III)_chloride': 'FeCl3','dihydrogen_phosphate': 'H2PO4','hydrogen_carbonate': 'HCO3',
+                   'mercury_(I)_oxide': 'Hg2O','mercury_(II)_oxide': 'HgO', 'hydronium': 'H3O',
+                   'hydrogen_phosphate': 'HPO42','hydrogen_sulfate': 'HSO4','hydroxide': 'OH',
+                   'ammonium': 'NH4', 'nitrate': 'NO3','nitrite': 'NO2',
+                   'permanganate': 'MnO4','peroxide': 'H2O22','sulfate': 'SO42',
+                   'sulfite': 'SO32', 'phosphate': 'PO43'}
+'''                                                                                           
+
+# Include element ions
+Zn2 = dict(formula= 'An2+', elements= 'Zn', name= 'Zinc', charge= '2')
+Cr3 = dict(formula= 'Cr3+', elements= 'Cr', name= 'Zinc', charge= '3')
+K1 = dict(formula= 'K1', elements= 'K', name= 'Potassium', charge= '1')
+Mg2 = dict(formula= 'Mg2+', elements= 'Mg', name= 'Magnesium', charge= '2')
+]
+'''
+k = C2H3O2['formula']
+print(k)
