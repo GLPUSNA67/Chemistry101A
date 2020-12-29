3318c38077c546ac441ab5b37e2c23ae96e50887
Gordon Pettus <38599393+GLPUSNA67@users.noreply.github.com>
2020-12-28 21:41:11 -0800

    Added display fields to the eci_ dictionaries to display display_temp_units, display_temp_qty, display_press_units,display_press_qty, display_qty = "", and display_units = "".

--- eciDict.py
+++ eciDict.py
@@ -1,20 +1,26 @@
 eci_1 = dict(eci = 'eci_1', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
-             qty = "", M_qty = "" , mass = "", Oxidation_State ="", units = "", valence = "",
+             display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
+             display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
 eci_2 = dict(eci = 'eci_2', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
-             qty = "", M_qty = "" , mass = "", Oxidation_State ="", units = "", valence = "",
+             display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
+             display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
 eci_3 = dict(eci = 'eci_3', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
-             qty = "", M_qty = "" , mass = "", Oxidation_State ="", units = "", valence = "",
+             display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
+             display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
 eci_4 = dict(eci = 'eci_4', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
-             qty = "", M_qty = "" , mass = "", Oxidation_State ="", units = "", valence = "",
+             display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
+             display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
 eci_5 = dict(eci = 'eci_5', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
-             qty = "", M_qty = "" , mass = "", Oxidation_State ="", units = "", valence = "",
+             display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
+             display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
 eci_6 = dict(eci = 'eci_6', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
-             qty = "", M_qty = "" , mass = "", Oxidation_State ="", units = "", valence = "",
+             display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
+             display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
 
 eci_db = {}
