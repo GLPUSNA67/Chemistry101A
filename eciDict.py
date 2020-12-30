3dfe6bd5da7b2211635c58df5b9a63c1f23a2103
Gordon Pettus <38599393+GLPUSNA67@users.noreply.github.com>
2020-12-29 21:20:09 -0800

    Moved the structure logic to a doc string.

--- eciDict.py
+++ eciDict.py
@@ -1,3 +1,13 @@
+'''
+The following is a guess at what an eci dictionary will contain.
+It includes all the items needed for each frame of elements, compounds or ions.
+It will hold variables essential to a process.
+*** Everytime a new eci is selected, the dictionary for that frame will need to be reset
+so incorrect values are not retained. ***
+Also, when creating a database record of each step of the process, all of these items and
+all the other process related items will be added to a process dictionary, and that dictionary
+will be used to create a database record of each step of the process.
+'''
 eci_1 = dict(eci = 'eci_1', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
              display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
              display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
