236de8118b972464f638b753ec18f654ef571fef
Gordon Pettus <38599393+GLPUSNA67@users.noreply.github.com>
2021-01-05 07:57:00 -0800

    Renamed the dictionary to eci_d.

--- eciDict.py
+++ eciDict.py
@@ -8,35 +8,35 @@ Also, when creating a database record of each step of the process, all of these 
 all the other process related items will be added to a process dictionary, and that dictionary
 will be used to create a database record of each step of the process.
 '''
-eci_1 = dict(eci = 'eci_1', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
+eci_1_d = dict(eci = 'eci_1', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
              display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
              display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
-eci_2 = dict(eci = 'eci_2', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
+eci_2_d = dict(eci = 'eci_2', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
              display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
              display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
-eci_3 = dict(eci = 'eci_3', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
+eci_3_d = dict(eci = 'eci_3', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
              display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
              display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
-eci_4 = dict(eci = 'eci_4', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
+eci_4_d = dict(eci = 'eci_4', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
              display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
              display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
-eci_5 = dict(eci = 'eci_5', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
+eci_5_d = dict(eci = 'eci_5', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
              display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
              display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
-eci_6 = dict(eci = 'eci_6', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
+eci_6_d = dict(eci = 'eci_6', eci_type = "", name ="", column = "", electronegativity = "", _group = "",
              display_qty = "", qty = "", M_qty = "" , mass = "", Oxidation_State ="", display_units = "", units = "", valence = "",
              display_temp_units= "", display_temp_qty="", display_press_units= "", display_press_qty= "",
              temp_units= "", temp_qty="", press_units= "", press_qty= "")
 
-eci_db = {}
-eci_db['eci_1'] = eci_1
-eci_db['eci_2'] = eci_2
-eci_db['eci_3'] = eci_3
-eci_db['eci_4'] = eci_4
-eci_db['eci_5'] = eci_5
-eci_db['eci_6'] = eci_6
+eci_d = {}
+eci_d['eci_1'] = eci_1_d
+eci_d['eci_2'] = eci_2_d
+eci_d['eci_3'] = eci_3_d
+eci_d['eci_4'] = eci_4_d
+eci_d['eci_5'] = eci_5_d
+eci_d['eci_6'] = eci_6_d
